8def72c3f499f7c031f646dadac459666c87a84c
Erick Sikina Duarte <erickduartesikina@gmail.com>
2024-09-23 08:54:54 -0300

    xD

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'smartwash';
+  ngOnInit() {}
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HlmButtonDirective,
+  ],
+  providers: [provideHttpClient(withFetch()), CookieService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
