24569cbd89905b90c1564fb80ed10272068adc67
Erick Sikina Duarte <erickduartesikina@gmail.com>
2024-09-24 18:08:06 -0300

    feat: criar reponse de endereco

---
+++ src/app/core/models/enderecos/responses/EnderecoResponse.ts
@@ -0,0 +1,10 @@
+export interface EnderecoResponse {
+  id: string;
+  rua: string;
+  numero: string;
+  bairro: string;
+  cep: string;
+  cidade: string;
+  estado: string;
+  complemento: string;
+}
