3665475ebbee206a8e0fdb18ada4a5b282a627e2
Erick Sikina Duarte <erickduartesikina@gmail.com>
2024-10-02 22:04:21 -0300

    feat: transformar interface em classe

--- src/app/core/models/clientes/requests/ClienteRequest.ts
+++ src/app/core/models/clientes/requests/ClienteRequest.ts
@@ -1,11 +1,11 @@
 import { EnderecoRequest } from '../../enderecos/requests/EnderecoRequest';
 import { Resposta } from '../../enums/Resposta';
 
-export interface ClienteResponse {
-  nome: string;
-  cpf: string;
-  celular: string;
-  email: string;
-  desejaSerNotificado: Resposta;
-  endereco: EnderecoRequest;
+export class ClienteRequest {
+  nome!: string;
+  cpf!: string;
+  celular!: string;
+  email!: string;
+  desejaSerNotificado!: Resposta;
+  endereco!: EnderecoRequest;
 }
